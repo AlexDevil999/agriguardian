1b4c7656d5153061f1054e1ef4002b7cf8b7d928
michael.kutsenko1 <michael.kutsenko@fordewind.io>
2021-07-16 15:04:19 +0300

    swagger confi

--- src/main/java/com/agriguardian/config/SwaggerConfig.java
+++ src/main/java/com/agriguardian/config/SwaggerConfig.java
@@ -2,9 +2,11 @@ package com.agriguardian.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -13,21 +15,41 @@ import java.util.Collections;
 
 @Configuration
 @EnableSwagger2
-public class SwaggerConfig {
+public class SwaggerConfig  implements WebMvcConfigurer {
+//    @Bean
+//    public Docket api() {
+//        return new Docket(DocumentationType.SWAGGER_2)
+//                .select()
+//                .apis(RequestHandlerSelectors.basePackage("com.agriguardian.controller"))
+//                .paths(PathSelectors.any())
+//                .build().apiInfo(new ApiInfo(
+//                        "Cloud API",
+//                        "Agriguardian API",
+//                        "ver 1.0.0.1",
+//                        null,
+//                        null,
+//                        null,
+//                        null,
+//                        Collections.emptyList()));
+//    }
+
     @Bean
     public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select()
+        return new Docket(DocumentationType.SWAGGER_2).select()
                 .apis(RequestHandlerSelectors.basePackage("com.agriguardian.controller"))
                 .paths(PathSelectors.any())
-                .build().apiInfo(new ApiInfo(
-                        "Cloud API",
-                        "Agriguardian API.",
-                        "ver 1.0.0.1",
-                        null,
-                        null,
-                        null,
-                        null,
-                        Collections.emptyList()));
+                .build().apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfo(
+                "Cloud API",
+                "Agriguardian API",
+                "1.0",
+                null,
+                new Contact("", "", "email@email.com"),
+                "API",
+                "API license URL",
+                Collections.emptyList());
     }
 }
