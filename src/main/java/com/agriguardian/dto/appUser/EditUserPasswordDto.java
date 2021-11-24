91f022714b4bfe27bc4b5df6c694e4b4b2a8e004
Vladyslav Zabrodskyi <vlad_zab_evg@Ukr.net>
2021-11-24 09:03:51 +0200

    notNull on passwordDto

--- src/main/java/com/agriguardian/dto/appUser/EditUserPasswordDto.java
+++ src/main/java/com/agriguardian/dto/appUser/EditUserPasswordDto.java
@@ -5,11 +5,15 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.validation.constraints.NotNull;
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
 public class EditUserPasswordDto {
+    @NotNull(message = "field 'newPassword' is mandatory")
     private String newPassword;
+    @NotNull(message = "field 'oldPassword' is mandatory")
     private String oldPassword;
 }
